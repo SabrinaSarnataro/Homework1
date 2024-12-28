56b2609a92f825dd33724f7cec097ff625409ba1
SabrinaSarnataro <steelesabrina@hotmail.it>
2024-12-28 10:46:04 +0100

    arm_controller_node.cpp

--- arm_controller/src/arm_controller_node.cpp
+++ arm_controller/src/arm_controller_node.cpp
@@ -10,23 +10,24 @@ public:
     {
         // Publisher per inviare comandi di posizione al controller
         position_publisher_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/JointPositionController/commands", 10);
+        timer_ = this->create_wall_timer(
+             500ms, std::bind(&ArmController::positionCommandCallback, this));
         
         // Subscriber per ricevere lo stato delle giunture
         joint_state_subscriber_ = this->create_subscription<sensor_msgs::msg::JointState>(
             "/joint_states", 10,
             std::bind(&ArmControlNode::jointStateCallback, this, std::placeholders::_1));
         
-        // Subscriber per ricevere comandi di posizione da linea di comando
-        position_command_subscriber_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
-            "/JointPositionController/commands", 10,
-            std::bind(&ArmControlNode::positionCommandCallback, this, std::placeholders::_1));
         
         RCLCPP_INFO(this->get_logger(), "Arm Control Node started and ready for external position commands.");
     }
 
 private:
-    void positionCommandCallback(const std_msgs::msg::Float64MultiArray::SharedPtr msg)
+    void positionCommandCallback()
     {
+        auto msg = std_msgs::msg::Float64MultiArray();
+        msg.data = {0.0, 0.5, 1.0, 0.5};
+        
         // Invia il comando di posizione ricevuto al controller
         position_publisher_->publish(*msg);
         RCLCPP_INFO(this->get_logger(), "Position command received and published.");
@@ -41,9 +42,10 @@ private:
         }
     }
 
+    rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr position_publisher_;
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_state_subscriber_;
-    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr position_command_subscriber_;
+
 };
 
 int main(int argc, char *argv[])
